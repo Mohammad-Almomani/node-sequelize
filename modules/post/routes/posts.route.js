f535abb498ab137e833507d14c76819b55893caa
Mohammad Almomani <mohammad.almomani@opensooq.com>
2024-04-02 15:29:54 +0300

    handled new routes

---
+++ modules/post/routes/posts.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { getPosts, getOnePost, addPost } = require('../controllers/post.controller');
+
+const router = express.Router();
+
+// GET /posts
+router.get('/', getPosts);
+
+// GET /posts/:id
+router.get('/:id', getOnePost);
+
+router.post('/', addPost);
+
+module.exports = router;
