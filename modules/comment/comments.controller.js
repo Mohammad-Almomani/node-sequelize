9e38bfc8e2a57913fdff7a501ca359b6e1da1e9f
Mohammad Almomani <mohammad.almomani@opensooq.com>
2024-04-04 11:43:29 +0300

    added comment owner and associated post

--- modules/comment/comments.controller.js
+++ modules/comment/comments.controller.js
@@ -1,33 +1,43 @@
-const { Comment } = require('../../models');
+const { Comment, Post, User } = require("../../models");
 
 const getAllComments = async (req, res, next) => {
-    try {
-        const comments = await Comment.findAll();
-        res.json(comments);
-    } catch (error) {
-        next(error);
-    }
-    }
+  try {
+    const comments = await Comment.findAll();
+    res.json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
 
 const createComment = async (req, res, next) => {
-    try {
-      const { content, userId, postId } = req.body;
-      const comment = await Comment.create({ content, userId, postId });
-      res.status(201).json(comment);
-    } catch (error) {
-      next(error);
-    }
-  };
-  
-  // Controller function to get all comments for a specific post
-  const getCommentsByPostId = async (req, res, next) => {
-    try {
-      const { postId } = req.params;
-      const comments = await Comment.findAll({ where: { postId } });
-      res.json(comments);
-    } catch (error) {
-      next(error);
-    }
-  };
-  
-  module.exports = { createComment, getCommentsByPostId, getAllComments };
+  try {
+    const { content, userId, postId } = req.body;
+    const comment = await Comment.create({ content, userId, postId });
+    res.status(201).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Controller function to get all comments for a specific post
+const getCommentsByPostId = async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+    const comments = await Comment.findAll({
+      where: { postId },
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["firstName", "lastName", "email"],
+        },
+        { model: Post, as: "post" },
+      ],
+    });
+    res.json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createComment, getCommentsByPostId, getAllComments };
