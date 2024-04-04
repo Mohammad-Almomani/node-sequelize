219359632b5cf96c443b350526e7c5436cc71c43
Mohammad Almomani <mohammad.almomani@opensooq.com>
2024-04-04 15:12:25 +0300

    error handler

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/comments", commentsRoute);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-    res.status(500).send("Something broke!");
+    res.status(500).send(err.message);
 });
 
 app.get("/", (req, res) => {
